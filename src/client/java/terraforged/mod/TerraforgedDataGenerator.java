136c9f436c0cdc3df78120d624e4c39e8efe5b1c
Abinnovator <abtheinnovator@gmail.com>
2025-09-07 17:52:54 +0530

    biome trial

--- src/client/java/terraforged/mod/TerraforgedDataGenerator.java
+++ src/client/java/terraforged/mod/TerraforgedDataGenerator.java
@@ -2,10 +2,32 @@ package terraforged.mod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.world.biome.Biome;
+import terraforged.mod.world.ModBiomes;
 
 public class TerraforgedDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
-	}
+
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        // This will hook your biome bootstrap into the generator
+        pack.addProvider((output, registriesFuture) ->
+                new FabricDynamicRegistryProvider(output, registriesFuture) {
+                    @Override
+                    protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+                        ModBiomes.bootstrapDatagen(entries); // <-- datagen version
+                    }
+
+                    @Override
+                    public String getName() {
+                        return "Terraforged Biomes";
+                    }
+                });
+
+    }
 }
--- src/main/java/terraforged/mod/world/ModBiomes.java
+++ src/main/java/terraforged/mod/world/ModBiomes.java
@@ -1,16 +1,56 @@
 package terraforged.mod.world;
 
+import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeEffects;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.biome.SpawnSettings;
 import terraforged.mod.Terraforged;
 
-public class ModBiomes {
-    // This creates a "key" that we can use to refer to our biome
-    public static final RegistryKey<Biome> DENSE_JUNGLE = RegistryKey.of(RegistryKeys.BIOME,
-            Identifier.of(Terraforged.MOD_ID, "dense_jungle"));
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 
+public class ModBiomes {
+
+    // Create biome key
+    public static final RegistryKey<Biome> DENSE_JUNGLE = RegistryKey.of(
+            RegistryKeys.BIOME,
+             Identifier.of(Terraforged.MOD_ID, "dense_jungle")
+    );
+
+    // Runtime bootstrap (used when Minecraft loads)
+    public static void bootstrap(Registerable<Biome> context) {
+        context.register(DENSE_JUNGLE, createDenseJungle());
+    }
+
+    // Datagen bootstrap (used by TerraforgedDataGenerator)
+    public static void bootstrapDatagen(FabricDynamicRegistryProvider.Entries entries) {
+        entries.add(DENSE_JUNGLE, createDenseJungle());
+    }
+
+    // Create the actual biome object
+    private static Biome createDenseJungle() {
+        SpawnSettings.Builder spawnBuilder = new SpawnSettings.Builder();
+        GenerationSettings.Builder generationBuilder = new GenerationSettings.Builder();
+
+        return new Biome.Builder()
+                .precipitation(true)
+                .temperature(0.95f)
+                .downfall(0.9f)
+                .effects(new BiomeEffects.Builder()
+                        .skyColor(7842047)
+                        .fogColor(12638463)
+                        .waterColor(4159204)
+                        .waterFogColor(329011)
+                        .build())
+                .spawnSettings(spawnBuilder.build())
+                .generationSettings(generationBuilder.build())
+                .build();
+    }
+
+    // Debug / logging hook
     public static void registerBiomes() {
         Terraforged.LOGGER.info("Registering Biomes for " + Terraforged.MOD_ID);
     }
